756b39fd00a7a3b36425c98245853ed0ad00e315
Mason Proffitt <mason.proffitt@cern.ch>
2021-03-23 15:34:44 +0100

    fix task 5 dimuon mass range

--- tasks/5/rdataframe.C
+++ tasks/5/rdataframe.C
@@ -22,7 +22,7 @@ void rdataframe() {
     ROOT::RDataFrame df("Events", "root://eospublic.cern.ch//eos/root-eos/benchmark/Run2012B_SingleMu.root");
     auto h = df.Filter("nMuon >= 2")
                .Define("Dimuon_mass", compute_dimuon_masses, {"Muon_pt", "Muon_eta", "Muon_phi", "Muon_mass", "Muon_charge"})
-               .Filter("Sum(Dimuon_mass > 60 && Dimuon_mass < 100) > 0")
+               .Filter("Sum(Dimuon_mass > 60 && Dimuon_mass < 120) > 0")
                .Histo1D({"", ";MET (GeV);N_{Events}", 100, 0, 2000}, "MET_pt");
 
     TCanvas c;
