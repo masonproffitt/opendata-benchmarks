80a70c0d8f93d5cd23cf97632829e3b518d0b8d0
Mason Proffitt <mason.proffitt@cern.ch>
2021-03-24 18:38:29 +0100

    fix task 7

--- tasks/7/rdataframe.C
+++ tasks/7/rdataframe.C
@@ -1,40 +1,41 @@
-ROOT::RVec<int> good_jets(const ROOT::RVec<float> &eta1,
-                          const ROOT::RVec<float> &phi1,
-                          const ROOT::RVec<float> &pt2,
-                          const ROOT::RVec<float> &eta2,
-                          const ROOT::RVec<float> &phi2) {
-    ROOT::RVec<int> mask(eta1.size(), 1);
-    if (eta2.size() == 0) {
-        return mask;
+ROOT::RVec<int> get_jet_lepton_isolation(const ROOT::RVec<float> &jet_eta,
+                                         const ROOT::RVec<float> &jet_phi,
+                                         const ROOT::RVec<float> &lepton_eta,
+                                         const ROOT::RVec<float> &lepton_phi) {
+    ROOT::RVec<int> jet_mask(jet_eta.size(), 1);
+    if (jet_eta.size() == 0 || lepton_eta.size() == 0) {
+        return jet_mask;
     }
 
-    const auto eta3 = eta2[pt2 > 10];
-    const auto phi3 = phi2[pt2 > 10];
-    if (eta3.size() == 0) {
-        return mask;
+    const auto jet_lepton_indices = ROOT::VecOps::Combinations(jet_eta, lepton_eta);
+    for (auto i = 0; i < jet_lepton_indices[0].size(); ++i) {
+        const auto jet_idx = jet_lepton_indices[0][i];
+        const auto lepton_idx = jet_lepton_indices[1][i];
+        const auto deltaR = ROOT::VecOps::DeltaR(jet_eta[jet_idx], jet_phi[jet_idx], lepton_eta[lepton_idx], lepton_phi[lepton_idx]);
+        if (deltaR < 0.4) {
+            jet_mask[jet_idx] = 0;
+        }
     }
-
-    const auto c = ROOT::VecOps::Combinations(eta1, eta3);
-    for (auto i = 0; i < c[0].size(); i++) {
-        const auto i1 = c[0][i];
-        const auto i2 = c[1][i];
-        const auto dr = ROOT::VecOps::DeltaR(eta1[i1], phi1[i1], eta2[i2], phi2[i2]);
-        if (dr < 0.4) mask[i1] = 0;
-    }
-    return mask;
+    return jet_mask;
 }
 
 
 void rdataframe() {
     ROOT::EnableImplicitMT();
     ROOT::RDataFrame df("Events", "root://eospublic.cern.ch//eos/root-eos/benchmark/Run2012B_SingleMu.root");
-    auto h = df.Filter("nJet >= 1")
-               .Define("goodJet_antiMuon", good_jets, {"Jet_eta", "Jet_phi", "Muon_pt", "Muon_eta", "Muon_phi"})
-               .Define("goodJet_antiElectron", good_jets, {"Jet_eta", "Jet_phi", "Electron_pt", "Electron_eta", "Electron_phi"})
-               .Define("goodJet", "Jet_pt > 30 && goodJet_antiMuon && goodJet_antiElectron")
-               .Filter("Sum(goodJet) > 0")
-               .Define("goodJet_sumPt", "Sum(Jet_pt[goodJet])")
-               .Histo1D({"", ";Jet p_{T} sum (GeV);N_{Events}", 100, 15, 200}, "goodJet_sumPt");
+    auto h = df.Define("Lepton_pt", "Concatenate(Electron_pt, Muon_pt)")
+               .Define("Lepton_eta", "Concatenate(Electron_eta, Muon_eta)")
+               .Define("Lepton_phi", "Concatenate(Electron_phi, Muon_phi)")
+               .Define("goodLepton", "Lepton_pt > 10")
+               .Define("goodLepton_eta", "Lepton_eta[goodLepton]")
+               .Define("goodLepton_phi", "Lepton_phi[goodLepton]")
+               .Define("goodJet", "Jet_pt > 30")
+               .Define("goodJet_pt", "Jet_eta[goodJet]")
+               .Define("goodJet_eta", "Jet_eta[goodJet]")
+               .Define("goodJet_phi", "Jet_eta[goodJet]")
+               .Define("goodJet_leptonIsolation", get_jet_lepton_isolation, {"goodJet_eta", "goodJet_phi", "goodLepton_eta", "goodLepton_phi"})
+               .Define("goodIsolatedJet_sumPt", "Sum(goodJet_pt[goodJet_leptonIsolation])")
+               .Histo1D({"", ";Jet p_{T} sum (GeV);N_{Events}", 100, 15, 200}, "goodIsolatedJet_sumPt");
 
     TCanvas c;
     h->Draw();
