f8ca932af1bdb68c730fd14e2b789d844248ea52
Mason Proffitt <mason.proffitt@cern.ch>
2021-03-24 18:31:25 +0100

    clean up task 5

--- tasks/5/rdataframe.C
+++ tasks/5/rdataframe.C
@@ -1,28 +1,31 @@
-ROOT::RVec<float> compute_dimuon_masses(const ROOT::RVec<float> &pt,
-                                        const ROOT::RVec<float> &eta,
-                                        const ROOT::RVec<float> &phi,
-                                        const ROOT::RVec<float> &mass,
-                                        const ROOT::RVec<int> &charge) {
-    ROOT::RVec<float> dimuon_masses;
-    const auto c = ROOT::VecOps::Combinations(pt, 2);
-    for (auto i = 0; i < c[0].size(); i++) {
-        const auto i1 = c[0][i];
-        const auto i2 = c[1][i];
-        if (charge[i1] == charge[i2]) continue;
-        const ROOT::Math::PtEtaPhiMVector v1(pt[i1], eta[i1], phi[i1], mass[i1]);
-        const ROOT::Math::PtEtaPhiMVector v2(pt[i2], eta[i2], phi[i2], mass[i2]);
-        dimuon_masses.push_back((v1 + v2).mass());
-    }
-    return dimuon_masses;
+ROOT::RVec<ROOT::Math::PtEtaPhiMVector> make_p4(const ROOT::RVec<float> &pt,
+                                                const ROOT::RVec<float> &eta,
+                                                const ROOT::RVec<float> &phi,
+                                                const ROOT::RVec<float> &mass) {
+    return ROOT::VecOps::Map(pt, eta, phi, mass, [](const float pt, const float eta, const float phi, const float mass){ return ROOT::Math::PtEtaPhiMVector(pt, eta, phi, mass); });
 }
 
+ROOT::RVec<float> get_dimuon_mass(const ROOT::RVec<ROOT::Math::PtEtaPhiMVector> &muon_p4,
+                                  const ROOT::RVec<int> &muon_charge) {
+    ROOT::RVec<float> dimuon_mass;
+    const auto dimuon_indices = ROOT::VecOps::Combinations(muon_p4, 2);
+    for (auto i = 0; i < dimuon_indices[0].size(); ++i) {
+        const auto muon_idx0 = dimuon_indices[0][i];
+        const auto muon_idx1 = dimuon_indices[1][i];
+        if (muon_charge[muon_idx0] != muon_charge[muon_idx1]) {
+            dimuon_mass.push_back((muon_p4[muon_idx0] + muon_p4[muon_idx1]).mass());
+        }
+    }
+    return dimuon_mass;
+}
 
 void rdataframe() {
     ROOT::EnableImplicitMT();
     ROOT::RDataFrame df("Events", "root://eospublic.cern.ch//eos/root-eos/benchmark/Run2012B_SingleMu.root");
     auto h = df.Filter("nMuon >= 2")
-               .Define("Dimuon_mass", compute_dimuon_masses, {"Muon_pt", "Muon_eta", "Muon_phi", "Muon_mass", "Muon_charge"})
-               .Filter("Sum(Dimuon_mass > 60 && Dimuon_mass < 120) > 0")
+               .Define("Muon_p4", make_p4, {"Muon_pt", "Muon_eta", "Muon_phi", "Muon_mass"})
+               .Define("Dimuon_mass", get_dimuon_mass, {"Muon_p4", "Muon_charge"})
+               .Filter("Any(60 < Dimuon_mass && Dimuon_mass < 120)")
                .Histo1D({"", ";MET (GeV);N_{Events}", 100, 0, 2000}, "MET_pt");
 
     TCanvas c;
